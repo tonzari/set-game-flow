1c792c7f870f3ed12b72adec5de3f9e8501c38ba
antonio <antoniomanzari@gmail.com>
2021-03-29 10:44:22 -0400

    init commit

---
+++ Assets/Player.cs
@@ -0,0 +1,11 @@
+﻿[System.Serializable]
+public class Player
+{
+    public string PlayerName { get; set; }
+    public int Score { get; set; }
+
+    public Player(string playerName)
+    {
+        PlayerName = playerName;
+    }
+}
